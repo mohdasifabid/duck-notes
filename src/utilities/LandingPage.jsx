fcec53821a15a44d6e4852d0a6aa236b27cca558
asif <masifabid@outlook.com>
2022-06-10 02:32:47 +0530

    Desktop view leftbar added

--- src/utilities/LandingPage.jsx
+++ src/utilities/LandingPage.jsx
@@ -4,6 +4,7 @@ import "./LandingPage.css";
 import { Navbar } from "./Navbar";
 import { NoteCard } from "./NoteCard";
 import { NoteMaker } from "./NoteMaker";
+import { Link } from "react-router-dom";
 
 export const LandingPage = () => {
   const { state, dispatch } = useNote();
@@ -19,18 +20,53 @@ export const LandingPage = () => {
   const filteredNotes = searchNoteFunction(state.notes, state.searchQuery);
 
   return (
-    <div>
+    <div className="common-big-container">
       <Navbar />
-      <div className="landing-page-body">
-        <div className="landing-page-note-maker-container">
-          <NoteMaker />
+      <div className="main-body">
+        <div class="leftbar-container">
+          <Link to="/" className="listbar-links">
+            <span className="leftbar-icon-name">
+              <i className="list-bar-icons fa-solid fa-home"></i>
+              Home
+            </span>
+          </Link>
+          <Link to="/labels" className="listbar-links">
+            <span className="leftbar-icon-name">
+              <i className="list-bar-icons fa-solid fa-tag"></i>
+              Labels
+            </span>
+          </Link>
+          <Link to="/archive" className="listbar-links">
+            <span className="leftbar-icon-name">
+              <i className="list-bar-icons fa-solid fa-box-archive"></i>
+              Archive
+            </span>
+          </Link>
+          <Link to="/archive" className="listbar-links">
+            <span className="leftbar-icon-name">
+              <i class="list-bar-icons fa-solid fa-trash-can"></i>
+              Trash
+            </span>
+          </Link>
+          <Link to="/profile" className="listbar-links">
+            <span className="leftbar-icon-name">
+              <i className="list-bar-icons fa-solid fa-user"></i>
+              Profile
+            </span>
+          </Link>
         </div>
-        <div className="landing-page-note-cards-container">
-          {filteredNotes.map((item) => {
-            return <NoteCard item={item} key={item._id} type="newNote" />;
-          })}
+        <div className="landing-page-body">
+          <div className="landing-page-note-maker-container">
+            <NoteMaker />
+          </div>
+          <div className="landing-page-note-cards-container">
+            {filteredNotes.map((item) => {
+              return <NoteCard item={item} key={item._id} type="newNote" />;
+            })}
+          </div>
         </div>
       </div>
+
       <Footer />
     </div>
   );

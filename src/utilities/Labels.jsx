dd3f25944ca37a261bd38382d9e209f7ee960e4e
asif <masifabid@outlook.com>
2022-06-19 12:26:05 +0530

    labels populated acc to tag

--- src/utilities/Labels.jsx
+++ src/utilities/Labels.jsx
@@ -1,18 +1,35 @@
 import { useNote } from "../useNote";
 import { NoteCard } from "./NoteCard";
 import { Layout } from "./Layout";
+import React, { useEffect } from "react";
 
 export const LabelsPage = () => {
   const { state } = useNote();
-
+  const tagged = () => {
+    let notesArr = state.notes;
+    let obj = {};
+    for (let i = 0; i < notesArr.length; i++) {
+      if (obj[notesArr[i].tag] === undefined) {
+        obj[notesArr[i].tag] = [notesArr[i]];
+      } else {
+        obj[notesArr[i].tag] = [...obj[notesArr[i].tag], notesArr[i]];
+      }
+    }
+    return obj;
+  };
+  let newObj = tagged();
   return (
     <Layout>
       <div className="archive-page-body">
-        {state.notes.map((item) => {
+        {Object.keys(newObj).map((lab) => {
           return (
             <>
-              <p>{item.tag}</p>
-              <NoteCard type="label" item={item} key={item._id} />
+              <p>
+                <strong>{lab}</strong>
+              </p>
+              {newObj[lab].map((item) => {
+                return <NoteCard type="label" item={item} key={item._id} />;
+              })}
             </>
           );
         })}

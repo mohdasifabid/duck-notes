a38c535e3d5fd83be0ae8ff77fb8ca403ec37468
asif <masifabid@outlook.com>
2022-07-08 11:02:53 +0530

    getCall removed

--- src/utilities/ProfilePage.jsx
+++ src/utilities/ProfilePage.jsx
@@ -1,22 +1,6 @@
-import { useNote } from "../useNote";
-import { useEffect } from "react";
-import axios from "axios";
 import { Layout } from "./Layout";
 
 export const ProfilePage = ({ item }) => {
-  const { state } = useNote();
-  useEffect(() => {
-    const token = localStorage.getItem("encodedToken");
-    const getUser = async () => {
-      const response = await axios.get("/api/user", {
-        headers: {
-          authorization: token,
-        },
-      });
-    };
-    getUser();
-  }, []);
-
   return (
     <Layout>
       <div className="archive-page-body">

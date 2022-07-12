c8bab9342b57c87585172b0ffb7c84066ab1b2f0
Mohd Asif <masifabid@outlook.com>
2022-07-12 18:19:34 +0530

    bug-fixed: navigated to login page on every render

--- src/utilities/PrivateRoute.jsx
+++ src/utilities/PrivateRoute.jsx
@@ -1,9 +1,8 @@
 import {  Outlet, Navigate } from "react-router-dom";
-import { useAuthProvider } from "./../authProvider";
 
 export const PrivateRoute = () => {
-  const { state } = useAuthProvider();
-  if (state.isLoggedIn) {
+  const token = localStorage.getItem("encodedToken");
+  if (token) {
     return <Outlet />;
   } else {
     return <Navigate to="/login"/>;
